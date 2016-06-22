277520484bd43dc0001411aa087187cb170055ac
amrdruid <amrdruid@gmail.com>
2016-06-22 03:50:35 +0200

    Added response function to Bot model

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -2,6 +2,11 @@ class Bot < ActiveRecord::Base
 	 def self.search_words number, words
 	 	CLIENT.search(words, lang: "en").take(number).each{|t|
 	 		User.create(name: t.user.screen_name, tweet_id: t.id.to_s, tweet: t.text)
+	 		CLIENT.update(Bot.respond(t.user.screen_name), in_reply_to_status_id: t.id)
 	 	} 
-	 end 
+	 end
+
+	  def self.respond name 
+	  	"@" + name + " " + Response.offset(rand(Response.count)).message 
+	  end 
 end
